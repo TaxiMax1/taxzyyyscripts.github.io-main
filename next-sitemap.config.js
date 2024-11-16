49fa08f2ad95952c2f5aeafb84884aaff1a35357
TaxiMax1 <taximaxi1808@gmail.com>
2024-11-17 00:55:38 +0100

    url update
    url update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://txzscripts.dev/',
+  siteUrl: 'https://docs.taxzyyyscripts.com/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: './out',
